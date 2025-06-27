776b6abfdf9b6738746b5e9258a90917dd9b132b
sameerm14 <sameernadaf5761@gmail.com>
2025-06-27 10:19:22 +0530

    added experience

--- src/app/my-experiance/my-experiance.component.ts
+++ src/app/my-experiance/my-experiance.component.ts
@@ -17,6 +17,14 @@ export class MyExperianceComponent {
         'Choosing the right technologies and frameworks for our web project. We evaluate various options based on factors like project requirements, scalability, and team expertise.Design and Experience, Development, and Coding, which I learned. ',
       techStack: ['java', 'JavaScript', 'python', 'Mysql'],
     },
+{
+  company: 'Peerbits',
+  role: 'Back End Developer',
+  duration: '(Feb 2022 – Jun 2025)',
+  description:
+    'Backend Java Developer with 3.1 years of experience in designing, developing, and maintaining scalable Java-based web applications. Skilled in Spring Boot, REST APIs, Hibernate, and Microservices architecture. Experienced in MySQL and integrating third-party services like Stripe. Familiar with Postman, Git, Jenkins, and Agile methodology. Good understanding of API security, JWT authentication, and cloud deployment.',
+  techStack: ['Java', 'Spring Boot', 'REST APIs', 'Hibernate', 'MySQL', 'JWT', 'Git', 'Postman', 'Jenkins'],
+},
 
   ];
 }
